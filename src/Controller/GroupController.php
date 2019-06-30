90f114e61aa20667780a52e910f8b8a1fbbd2ae3
Vilius Maciulis <viliasas@rse.lt>
2019-07-01 01:10:31 +0300

    Created a action to retrieve full group list

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Group;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,11 +11,26 @@ class GroupController extends AbstractController
 {
 
     /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * GroupController constructor.
+     *
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
      * @Route("/groups", methods={"GET"}, name="group_list")
      */
     public function index()
     {
-        return $this->json([]);
+        return $this->json($this->entityManager->getRepository(Group::class)->findAll());
     }
 
 }
